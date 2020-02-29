52dca8f2f0aa991267a56bb75501fb9afab1df00
Chase Johnson <52802126+cc-johnson@users.noreply.github.com>
2020-02-29 17:15:21 -0600

    Update Interpolation.py

--- Group_Labs/Lab09/Interpolation.py
+++ Group_Labs/Lab09/Interpolation.py
@@ -1,10 +1,12 @@
+import random
+
 xList = [3, 4, 5, 7, 8]
 yList = [6, 7, 9, 3, 4]
 x = float(input("Type an x-value you would like to find: "))
 
-Counter = 0
-while x > xList[Counter]:
-    Counter += 1
+x_index = 0
+while x > xList[x_index]:
+    x_index += 1
 
-y = yList[Counter - 1] + (x - xList[Counter-1]) * ((yList[Counter] - yList[Counter - 1]) / (xList[Counter] - xList[Counter - 1]))
+y = yList[x_index - 1] + (x - xList[x_index-1]) * ((yList[x_index] - yList[x_index - 1]) / (xList[x_index] - xList[x_index - 1]))
 print(y)
